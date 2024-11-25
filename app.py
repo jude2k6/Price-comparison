ffe6a7235fe3858bc7c0c9287504dbcf90de506e
jude2k6 <61092177+jude2k6@users.noreply.github.com>
2024-11-25 23:14:34 +0000

    xhanged to upsate daily

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def run_update_script():
     subprocess.run(["python", script_path], check=True)
 
 
-scheduler.add_job(func=run_update_script, trigger="interval", seconds=60, id ="update", max_instances=1)
+scheduler.add_job(func=run_update_script, trigger="cron", hour=8, minute=0, id ="update", max_instances=1)
 
 
 if __name__ == '__main__':
