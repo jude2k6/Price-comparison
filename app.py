d7e0813dd7037795c419466e9080f2407e4e1ea2
jude2k6 <61092177+jude2k6@users.noreply.github.com>
2024-11-26 23:24:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def run_update_script():
     subprocess.run(["python", script_path], check=True)
 
 
-scheduler.add_job(func=run_update_script, trigger="cron", hour=23, minute = 10, id ="update", max_instances=1)
+scheduler.add_job(func=run_update_script, trigger="interval", minute = 3, id ="update", max_instances=1)
 
 
 if __name__ == '__main__':
     scheduler.start()
     print("Scheduled jobs:", scheduler.get_jobs())
     print("run")
-    app.run(port=81, debug=True, threaded=True)
+    #app.run(port=81, debug=True, threaded=True)
  
