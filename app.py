b44e89bdbcfcc38dc50aefd22c3e16fc4813c81f
jude2k6 <61092177+jude2k6@users.noreply.github.com>
2024-11-26 23:07:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def run_update_script():
     subprocess.run(["python", script_path], check=True)
 
 
-scheduler.add_job(func=run_update_script, trigger="cron", hour=23, minute = 5, id ="update", max_instances=1)
+scheduler.add_job(func=run_update_script, trigger="cron", hour=23, minute = 10, id ="update", max_instances=1)
 
 
 if __name__ == '__main__':
